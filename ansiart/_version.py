b27af256442dad0e0ed0e3dc823da3754d50abed
Kokecoco <gs2312160@gll-kaisei-s.sapporo-c.ed.jp>
2024-03-30 20:30:52 +0900

    1.0.2

--- ansiart/_version.py
+++ ansiart/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.1.dev1+g1ec4b80.d20240330'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev1', 'g1ec4b80.d20240330')
+__version__ = version = '1.0.3.dev0'
+__version_tuple__ = version_tuple = (1, 0, 3, 'dev0')
